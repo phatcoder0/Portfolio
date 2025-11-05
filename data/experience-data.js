eb3b6544536b0238d1fa1d84bb48e299edfed18b
phatcoder0 <thucode0@gmail.com>
2025-11-04 18:06:36 -0600

    experience-data.js

--- data/experience-data.js
+++ data/experience-data.js
@@ -2,24 +2,23 @@ export const experienceData = [
   { // Experience 1
     title: 'Web Developer',
     image: 'web-development.png', // Add your experience image to the "images-and-icons/images" folder and specify its filename here.
-    yrsOfExperience: '2023 - Present',
-    details: `Builds and showcases interactive web projects using HTML, CSS, JavaScript, and Node.js. 
+    yrsOfExperience: '2021 - Present',
+    details: `Builds and showcases interactive web projects using HTML, CSS, JavaScript. 
               Develops creative and responsive designs for various web applications.`
   },
   { // Experience 2 
-    title: 'Content Creator',
+    title: 'Cleaner',
     image: 'content-creator.png', // Add your experience image to the "images-and-icons/images" folder and specify its filename here.
-    yrsOfExperience: '2024 - Present',
-    details: `Creates and shares engaging web development content, tutorials, and design showcases. 
-              Focuses on teaching and inspiring others in the field of web development and UI/UX design.`
+    yrsOfExperience: '2022 – 2023',
+    details: `In 2022 and 2023, my family moved to the US and lived in California. At that time, I worked as a cleaner for several ADU houses and apartments.`
   },
   { // Experience 3
-    title: 'UI Designer',
+    title: 'Server',
     image: 'ui-designing.jpg', // Add your experience image to the "images-and-icons/images" folder and specify its filename here.
     yrsOfExperience: '2024 - Present',
-    details: `Designs modern and user-friendly interfaces with a focus on aesthetics and functionality. 
-              Utilizes design tools and front-end technologies to create seamless user experiences.`
+    details: `After graduating from high school, I worked as a waiter and cashier in many restaurants and fairs to gain more experience and income.`
   }
 
   // Feel free to add more experiences here.
 ];
+
