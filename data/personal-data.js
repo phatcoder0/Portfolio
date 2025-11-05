033c9fda176d73d5c1d0392baafcb3b10ca69a76
phatcoder0 <thucode0@gmail.com>
2025-11-04 18:14:04 -0600

    personal-data.js

--- data/personal-data.js
+++ data/personal-data.js
@@ -1,21 +1,22 @@
 export const personalData = {
-  name: 'Cute Meowish',
+  name: 'Phat Ly',
   profilePicture: 'profile pic2.jpg', // Add your photo to the  "images-and-icons/images" folder and specify its filename here
   resume: 'meowish resume.pdf',
   workAvailabilityStatus: 'Available for work',
   skills: ['Tiktoker', 'Web Developer', 'UI Designer'],
   adress: 'Somewhere, onEarth',
-  contactNumber: '1234567890',
-  email: 'meowish@gmail.com',
+  contactNumber: '714 703 9679',
+  email: 'lxp1626@gmail.com',
   emailPublicKey: 'YourPublicKey', // Go to emailJS.com, sign up, and get your public-key, then paste here
   serviceId: 'service_id', // Go to emailJS.com, sign up, and get your service-id then, paste here. 
   templateId: 'template_gqf06fp', // Go to emailJS.com, sign up, and get your template-id then, paste here. 
-  facebookLink: 'https:yourFacebookLink.com',
-  instagramLink: 'https:yourInstagramLink.com',
-  tiktokLink: 'https:yourTiktokLink.com',
+  facebookLink: 'https://www.facebook.com/e.bo.a.roi',
+  instagramLink: 'instagram.com/lxp_1606/',
+  tiktokLink: 'https://phatcoder0.github.io/Portfolio/',
   aboutMe: [
             `I am Meowish, a creative Web Designer & Developer from the vibrant world of Cat World. My passion lies in crafting visually striking and highly functional websites that help you stand out in your job search.`,
 
             `With over 2 years of experience, I specialize in blending aesthetics with seamless functionality, ensuring every project delivers an outstanding user experience.`
           ],
+
 }
